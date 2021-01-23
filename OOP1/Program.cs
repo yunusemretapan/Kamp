c469da08067787db8d735ad5148a57db4197ebbe
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-01-24 00:01:58 +0300

    Classes

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -11,12 +11,13 @@ namespace OOP1
                 throw new ArgumentNullException(nameof(args));
             }
 
-            Product product1 = new Product { Id = 1, CategoryId = 2, ProductName = "Masa", UnitPrice = 500, UnitsInStock = 3 };
+            Product product1 = new Product { Id = 1, CategoryId = 2, ProductName = "Table", UnitPrice = 500, UnitsInStock = 3 };
 
-            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice = 35, UnitsInStock = 5 };
+            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Pencil", UnitPrice = 35, UnitsInStock = 5 };
 
             ProductManager productManager = new ProductManager();
             productManager.Add(product1);
+            productManager.Add(product2);
         }
     }
 }
